7d8c3441d30b40789b05eb91f334251c332d2160
Kuiki <ikuiki360@gmail.com>
2019-06-18 10:34:16 +0800

    添加mysql与redis配置

--- conf/conf.go
+++ conf/conf.go
@@ -1,14 +1,24 @@
 package conf
 
 import (
+	"fmt"
+	"github.com/go-redis/redis"
 	"github.com/jinzhu/configor"
+	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
+
+	// init mysql
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 // Config 配置载体，所有可配置项都记载于此接口中
 type Config interface {
 	// Load 从文件中载入配置
 	Load(filename string) error
+	// GetMainDB 获取主DB
+	GetMainDB() (dataDB *gorm.DB)
+	// GetMainRedis 获取主Redis
+	GetMainRedis() (client *redis.Client)
 }
 
 // MustNewConfig 必须创建配置
@@ -20,6 +30,24 @@ func MustNewConfig() Config {
 	return c
 }
 
+// 数据库连接的配置详情
+type dbConf struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+}
+
+// Redis连接的配置详情
+type redisConf struct {
+	Host     string
+	Port     int
+	Password string
+	DBNo     int
+	PoolSize int
+}
+
 // NewConfig 创建配置
 func NewConfig() (c Config, err error) {
 	c = &config{}
@@ -28,8 +56,58 @@ func NewConfig() (c Config, err error) {
 
 // config Config接口的默认实现
 type config struct {
+	MainDBConf    dbConf
+	MainDB        *gorm.DB `json:"-"`
+	MainRedisConf redisConf
+	MainRedis     *redis.Client `json:"-"`
 }
 
-func (c *config) Load(filename string) error {
-	return errors.WithStack(configor.Load(c, filename))
+func (c *config) Load(filename string) (err error) {
+	err = errors.WithStack(configor.Load(c, filename))
+	if err != nil {
+		return err
+	}
+	err = errors.WithStack(c.RegisterMainDB())
+	if err != nil {
+		return err
+	}
+	err = errors.WithStack(c.RegisterMainRedis())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *config) RegisterMainDB() (err error) {
+	c.MainDB, err = gorm.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+			c.MainDBConf.User,
+			c.MainDBConf.Password,
+			c.MainDBConf.Host,
+			c.MainDBConf.Port,
+			c.MainDBConf.DBName,
+		))
+	return
+}
+
+func (c *config) RegisterMainRedis() (err error) {
+	mainRedis := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", c.MainRedisConf.Host, c.MainRedisConf.Port),
+		Password: c.MainRedisConf.Password,
+		DB:       c.MainRedisConf.DBNo,
+		PoolSize: c.MainRedisConf.PoolSize,
+	})
+	if err := mainRedis.Ping().Err(); err != nil {
+		return err
+	}
+	c.MainRedis = mainRedis
+	return nil
+}
+
+func (c *config) GetMainDB() (dataDB *gorm.DB) {
+	return c.MainDB
+}
+
+func (c *config) GetMainRedis() (client *redis.Client) {
+	return c.MainRedis
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,22 @@ module weassistant
 go 1.12
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
+	cloud.google.com/go v0.39.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20190515213511-eb9f6a1743f3 // indirect
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/jinzhu/configor v1.0.0
+	github.com/jinzhu/gorm v1.9.8
+	github.com/jinzhu/inflection v0.0.0-20190603042836-f5c5f50e6090 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/pkg/errors v0.8.1
-	gopkg.in/yaml.v2 v2.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5 // indirect
+	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
+	golang.org/x/sys v0.0.0-20190602015325-4c4f7f33c9ed // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.6.0 // indirect
 )
